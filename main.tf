120726848bb53671242ece93397ec1ae5cb2394d
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-05-05 14:55:35 +0530

    Completed

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform-Student-Instance"
